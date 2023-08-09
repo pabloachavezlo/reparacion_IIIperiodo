e7a84fbbfe68daea1ef39a06fcd6cf488e343e0a
Pablox <palchavez@scl.edu.gt>
2023-08-09 13:38:27 -0600

    Tarea ejercicio1

--- js/descripcion_del_producto.js
+++ js/descripcion_del_producto.js
@@ -1,29 +1,14 @@
-function addItemToDescripcion(item) {
-    const descripcion = document.querySelector('.descripcion_del_producto');
+let productos = document.querySelectorAll(".item");
 
-    // Crear un nuevo elemento para la descripción
-    const newItem = document.createElement('div');
-    newItem.innerHTML = item.innerHTML;
+function ver_descripcion () {
+    let caja_descripcion_DOM = document.querySelector(".descripcion_del_producto");
+    caja_descripcion_DOM.innerHTML = caja_descripcion(this.id);
 
-    // Verificar si el contenido ya existe en la descripción
-    const descripcionItems = descripcion.querySelectorAll('.item');
-    for (const descripcionItem of descripcionItems) {
-        if (descripcionItem.innerHTML === newItem.innerHTML) {
-            // Si ya existe, no se agrega nuevamente
-            return;
-        }
-    }
-
-    // Agregar el nuevo elemento a la descripción
-    descripcion.appendChild(newItem);
+    let main = document.querySelector("#main");
+    main.classList.add("ocultar");
 }
 
-// Obtener todos los elementos con la clase 'item'
-const items = document.querySelector('.item');
 
-// Agregar el event listener a cada elemento con la clase 'item'
-items.forEach((item) => {
-    item.addEventListener('click', function() {
-        addItemToDescripcion(item);
-    });
-});
+productos.forEach((cada_item) => {
+    cada_item.addEventListener("click", ver_descripcion);
+})
