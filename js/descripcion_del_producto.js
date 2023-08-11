7c466e8f4dac1512d90061c2191d0f03ce2c3d22
Pablox <palchavez@scl.edu.gt>
2023-08-11 12:42:18 -0600

    Actualización descipt diseño

--- js/descripcion_del_producto.js
+++ js/descripcion_del_producto.js
@@ -10,14 +10,21 @@ export function descripcion(data, index) {
     
         <section class="middle">
           <h1 class="titulo_principal">${data[index].titulo}</h1>
-          <h2 class="descripciones">${data[index].descripción}</h2>
-          <div class="precios">
+          <h2 class="descripcion">${data[index].descripción}</h2>
+          <div class="botones">
             <button class="precio-valor">Q100.00</button>
             <button class="comprar-btn">Comprar</button>
             <button class="do_it">Hagalo usted mismo</button>
           </div>
-          <ul class="lista">Herramientas: ${data[index].herramientas.map((herramienta) => `<li>${herramienta}</li>`).join("")}</ul>
-          <ul class="lista">Pasos: ${data[index].pasos.map((pasos) => `<li>${pasos}</li>`).join("")}</ul>
+          <ul class="lista">
+            <p>Herramientas: </p>${data[index].herramientas.map((herramienta) => 
+            `<li>${herramienta}</li>`).join("")}
+          </ul>
+          <ul class="lista2">
+            <p>Pasos: </p>${data[index].pasos.map((pasos) => 
+            `<li>${pasos}</li>`).join("")}
+          </ul>
+          
         </section>
       </div>
     </div>
